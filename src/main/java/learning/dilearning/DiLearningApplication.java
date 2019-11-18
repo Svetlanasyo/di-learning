72b77ed5636317150b8b7cd9de13d6f9a2863bde
Svetlana <svetlana.syo@gmail.com>
2019-11-18 19:07:39 +0100

    get Bean

--- src/main/java/learning/dilearning/DiLearningApplication.java
+++ src/main/java/learning/dilearning/DiLearningApplication.java
@@ -1,13 +1,18 @@
 package learning.dilearning;
 
+import learning.dilearning.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiLearningApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiLearningApplication.class, args);
-    }
 
+        ApplicationContext ctx = SpringApplication.run(DiLearningApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+        controller.hello();
+    }
 }
---
+++ src/main/java/learning/dilearning/controllers/MyController.java
@@ -0,0 +1,13 @@
+package learning.dilearning.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!");
+
+        return "foo";
+    }
+}
