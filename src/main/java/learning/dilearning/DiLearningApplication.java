6f59a2d41e0e33df14f00132d9365cac5dd61ed9
Svetlana <svetlana.syo@gmail.com>
2019-11-18 18:15:27 +0100

    Initial commit

---
+++ src/main/java/learning/dilearning/DiLearningApplication.java
@@ -0,0 +1,13 @@
+package learning.dilearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learning/dilearning/DiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.dilearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
